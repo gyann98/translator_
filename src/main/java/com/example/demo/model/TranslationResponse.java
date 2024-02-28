97a08ffd69a86b2e23902ec4f911573ae4c31d9b
Adity Shaw <adityshaw99@gmail.com>
2024-02-28 12:28:17 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/TranslationResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+public class TranslationResponse {
+    private String translation;
+
+	public String getTranslation() {
+		return translation;
+	}
+
+	public void setTranslation(String translation) {
+		this.translation = translation;
+	}
+
+    // Constructor, getters, and setters
+}
